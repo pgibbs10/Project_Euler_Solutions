43455b0de5ef66a17ec29d0e54726cc3975da753
Paul Gibbs <pwgibbs10@gmail.com>
2018-02-05 14:55:46 -0700

    Finished problem 2, created a calculator for even fibonacci numbers

--- project_euler_2.rb
+++ project_euler_2.rb
@@ -1,6 +1,15 @@
-count = 0
-iter_count = 1
-test_count = 1
+def even_fib_calc num
+	even_fib_array = []
+	fib_array = [1,2,3]
+	count = 2
 
-def fib(n)
-puts count
+	until(fib_array.last > num)
+		fib_array.push(fib_array[count] + fib_array[count-1])
+		even_fib_array << fib_array[count] if fib_array[count] % 2 == 0
+		count += 1
+	end
+
+	return even_fib_array.inject(:+) + 2
+end
+
+puts even_fib_calc(4_000_000)
